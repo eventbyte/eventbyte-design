55ab4d9013c18698c58ea2cbca4458aefdea9991
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-17 01:21:10 +0700

    Improve event model

--- models/event.js
+++ models/event.js
@@ -5,7 +5,8 @@ const event = {
   origin: {
     image: "https://meetup.com/ImpactByte/events/12345678/cover.jpg", // optional
     url: "https://meetup.com/ImpactByte/events/12345678", // required
-    site: "Meetup" // optional
+    site: "Meetup", // optional
+    organizer: ["Impact Byte"] // optional
   },
   title: "Impact Byte Hackathon Premiere", // required
   description: "All fun of premier hackathon for the first timer developers.", // optional
@@ -21,13 +22,7 @@ const event = {
     currency: "IDR" // optional
   },
   topics: [
-    // optional
-    "Competition",
-    "Science",
-    "Technology",
-    "Software",
-    "Developer",
-    "Hackathon"
+    ObjectId() // Topic
   ],
   schedule: {
     start: ISODate(), // required
@@ -42,17 +37,14 @@ const event = {
     map: "https://goo.gl/maps/2Frvba3Nrp82" // optional
   },
   savedBy: [
-    // User
-    ObjectId(),
-    ObjectId(),
-    ObjectId()
+    ObjectId() // User
   ],
   joinedBy: [], // later
   discussedBy: [], // later
   sponsored: {
     status: true, // optional
-    dateStart: ISODate(), // optional
-    dateEnd: ISODate() // optional
+    start: ISODate(), // optional
+    end: ISODate() // optional
   },
   url: {
     plain: "https://eventbyte.co/event/1", // optional
